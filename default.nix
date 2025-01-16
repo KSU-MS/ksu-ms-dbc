a48754444c2cb1996bfbe4177cf15305470362b8
ChanceCtrl <>
2025-01-15 21:00:12 -0500

    Project restructure
    nix maybe the end of me
    
    .

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@
 
 pkgs.stdenv.mkDerivation {
   name = "can_pkg";
+
+  src = ./gen_tool;
   
   # Some deps needed to generate the output DBC
   buildInputs = [
@@ -13,6 +15,7 @@ pkgs.stdenv.mkDerivation {
   # In this case, it will be the generated file
   installPhase = ''
     mkdir -p $out
-    python3 ./json_to_dbc.py ./can_descriptor.json $out/car Orion RMS_PM Megasquirt
+    python3 ./json_to_dbc.py ./can_descriptor.json ./dbc-output/car Orion RMS_PM Megasquirt
+    mv ./dbc-output/car.dbc $out/car.dbc
   '';
 }
--- flake.nix
+++ flake.nix
@@ -1,41 +1,72 @@
+# I hate nix
 {
   description = "A very basic flake that generates the DBCs by running a python script";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs }: let
+    pkg_overlay = final: prev: {
+      can_pkg = final.callPackage ./default.nix { };
+    };
+    custom_overlays = [ pkg_overlay ];
 
-    pythonEnv = pkgs.python312.withPackages(ps: [
-      ps.cantools
-    ]);
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ self.overlays.default ];
+    };
 
   in {
-    # We export this package so we can refrence its output in another flake as an overlay
-    packages = {
-      default = pkgs.callPackage ./default.nix {};
-    };
+    overlays.default = nixpkgs.lib.composeManyExtensions custom_overlays;
 
-    # This is so that the github build action can just use the nix script to ensure reproducablility and making my life easier lol
-    apps.default = {
-      type = "app";
-      program = "${pythonEnv}/bin/python";
+    packages.x86_64-linux = rec {
+      can_pkg = pkgs.can_pkg;
+      default = can_pkg;
     };
-
-    # I'm keeping the devshell for debug purposes
-    devShells.default = pkgs.mkShell {
-      name = "ksu_dbc";
-      # Some deps needed to generate the output DBC
-      buildInputs = [
-        pkgs.python312
-        pkgs.python312Packages.cantools
-      ];
-    };
-
-    overlays.default = self: super: {
-      default = super.callPackage ./default.nix {};
-    };
-  });
+  };
 }
+
+# It makes me cry
+
+# {
+#   description = "A very basic flake that generates the DBCs by running a python script";
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+#     flake-utils.url = "github:numtide/flake-utils";
+#   };
+#
+#   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
+#     pkgs = nixpkgs.legacyPackages.${system};
+#
+#     pythonEnv = pkgs.python312.withPackages(ps: [
+#       ps.cantools
+#     ]);
+#
+#   in {
+#     # We export this package so we can refrence its output in another flake as an overlay
+#     packages = {
+#       default = pkgs.callPackage ./default.nix {};
+#     };
+#
+#     # This is so that the github build action can just use the nix script to ensure reproducablility and making my life easier lol
+#     apps.default = {
+#       type = "app";
+#       program = "${pythonEnv}/bin/python";
+#     };
+#
+#     # I'm keeping the devshell for debug purposes
+#     devShells.default = pkgs.mkShell {
+#       name = "ksu_dbc";
+#       # Some deps needed to generate the output DBC
+#       buildInputs = [
+#         pkgs.python312
+#         pkgs.python312Packages.cantools
+#       ];
+#
+#       overlays.default = self: super: {
+#         default = super.callPackage ./default.nix {};
+#       };
+#     };
+#   });
+# }
