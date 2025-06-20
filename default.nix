399f871dde86e422bd63ed9089d18f8eb0376b15
ChanceCtrl <>
2025-06-20 10:50:14 -0400

    fuck this shit

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ pkgs.stdenv.mkDerivation {
   # In this case, it will be the generated file
   installPhase = ''
     mkdir -p $out
-    python3 ./json_to_dbc.py ./can_descriptor.json ./dbc-output/car Orion 20240625-Gen5-CAN-DB.dbc elcon Megasquirt
+    python3 ./json_to_dbc.py ./can_descriptor.json ./dbc-output/car Orion 20240625-Gen5-CAN-DB elcon Megasquirt messageNEW
     mv ./dbc-output/car.dbc $out/car.dbc
   '';
 }
