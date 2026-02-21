a5d83bdac97010df18fb3ac70c171d160687c6fe
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-02-20 20:10:40 -0500

    More flake updates

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# I hate nix
 {
   description = "A very basic flake that generates the DBCs by running a python script";
 
@@ -13,27 +12,28 @@
       self,
       nixpkgs,
     }:
-    flake-utils.lib.eachDefaultSystem (
+    # Default for all nixpkgs
+    let
+      pkg_overlay = final: prev: {
+        can_pkg = final.callPackage ./default.nix { };
+      };
+      custom_overlays = [ pkg_overlay ];
+    in
+    {
+      overlays.default = custom_overlays;
+    }
+
+    # Add a package export for each system
+    // flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkg_overlay = final: prev: {
-          can_pkg = final.callPackage ./default.nix { };
-        };
-        custom_overlays = [ pkg_overlay ];
-
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ pkg_overlay ];
         };
-
       in
       {
-        overlays.default = nixpkgs.lib.composeManyExtensions custom_overlays;
-
-        packages = {
-          default = pkgs.can_pkg;
-          can_pkg = pkgs.can_pkg;
-        };
+        packages.default = pkgs.can_pkg;
       }
     );
 }
